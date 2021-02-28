e6eb24dccb628e7907f22f8187e24038e7b09d9d
Pavel Abduramanov <pabduramanov@i-doit.com>
2021-02-28 18:44:42 +0100

    feat(presets): Created the webpacker - a tool to load the presets and configuration files according to environement

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,8 @@
 const path = require('path');
-const { merge } = require('webpack-merge');
-const loadPresets = require('./build-tools/load-presets');
+const webpacker = require("./build-tools/webpacker");
 
-module.exports = (env) => {
-  const modeConfig = require(`./build-tools/webpack.${env.mode}.config`)(env);
-  const sharedConfig = {
-    mode: env.mode || 'production',
-    output: {
-      path: path.resolve(__dirname, 'dist'),
-    },
-    resolve: {
-      extensions: ['.js'],
-    },
-  };
-  const presets = loadPresets(env);
-
-  return merge(sharedConfig, modeConfig, presets);
-};
+module.exports = webpacker(
+    {
+        typescript: ['all'],
+        'build-size-budget': ['development'],
+    });
